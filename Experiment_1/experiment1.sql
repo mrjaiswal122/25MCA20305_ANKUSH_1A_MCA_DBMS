59f7c052c4f562dc0f76f14d22c60322751dcf48
Ankush jaiswal <obviouslyankush@gmail.com>
2026-01-21 22:18:17 +0530

    added Experminet 2

--- Experiment_1/experiment1.sql
+++ Experiment_1/experiment1.sql
@@ -1,50 +1,50 @@
-CREATE TABLE books (
+CREATE TABLE department (
     book_id INT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     author VARCHAR(100) NOT NULL,
     available_copies INT CHECK (available_copies >= 0)
 );
 
-CREATE TABLE members (
+CREATE TABLE employee (
     member_id INT PRIMARY KEY,
     member_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE
 );
 
-CREATE TABLE book_issue (
-    issue_id INT PRIMARY KEY,
-    book_id INT REFERENCES books(book_id),
-    member_id INT REFERENCES members(member_id),
+CREATE TABLE project (
+    project_id INT PRIMARY KEY,
+    book_id INT REFERENCES department(book_id),
+    employee_id INT REFERENCES employee(member_id),
     issue_date DATE,
     return_date DATE
 );
 
-INSERT INTO books VALUES
+INSERT INTO department VALUES
 (1, 'DBMS Concepts', 'Silberschatz', 5),
 (2, 'Operating System', 'Galvin', 3);
 
-INSERT INTO members VALUES
+INSERT INTO employee VALUES
 (101, 'Amit Kumar', 'amit@gmail.com'),
 (102, 'Neha Sharma', 'neha@gmail.com');
 
--- BOOK ISSUE
-INSERT INTO book_issue(issue_id,book_id,member_id,issue_date,
+-- PROJECT ASSIGNMENT
+INSERT INTO project(project_id,book_id,employee_id,issue_date,
 return_date)
 VALUES
 (1001, 1, 101, '2025-01-10', NULL);
 
 
-SELECT * FROM books;
-SELECT * FROM members;
-SELECT * FROM book_issue;
+SELECT * FROM department;
+SELECT * FROM employee;
+SELECT * FROM project;
 
 --UPDATE AVAILABLE BOOKS
-UPDATE books
+UPDATE department
 SET available_copies = available_copies - 1
 WHERE book_id = 1;
 
 --Delete book 
-DELETE FROM books WHERE book_id = 2
+DELETE FROM department WHERE book_id = 2
 
 CREATE ROLE app_user
 WITH LOGIN PASSWORD 'ankudata';
